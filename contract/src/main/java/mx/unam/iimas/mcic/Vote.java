e0fe1dc176b3c5345e7ed2b465ebdf42f964a811
Victor Cruz <vicdan215@gmail.com>
2021-11-22 08:39:45 -0600

    Add Lombok annotations and specific JSON methods

--- contract/src/main/java/mx/unam/iimas/mcic/Vote.java
+++ contract/src/main/java/mx/unam/iimas/mcic/Vote.java
@@ -1,7 +1,28 @@
 package mx.unam.iimas.mcic;
 
+import com.owlike.genson.Genson;
+import lombok.*;
 import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
 
 @DataType
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Vote {
+
+    private static final Genson genson = new Genson();
+
+    @Property
+    private String value;
+
+    public String toJSONString() {
+        return genson.serialize(this);
+    }
+
+    public static Vote fromJSONString(String json) {
+        return genson.deserialize(json, Vote.class);
+    }
 }
