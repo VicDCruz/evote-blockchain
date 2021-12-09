62d622b0760f2d32a1917ef1abd902c06592b7c9
Victor Cruz <vicdan215@gmail.com>
2021-12-08 23:56:39 -0600

    Add encryption methods directly on JSON transformation

--- contract/src/main/java/mx/unam/iimas/mcic/utils/JsonMapper.java
+++ contract/src/main/java/mx/unam/iimas/mcic/utils/JsonMapper.java
@@ -2,6 +2,11 @@ package mx.unam.iimas.mcic.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
+import static mx.unam.iimas.mcic.utils.AsymmetricCryptographicHelper.decrypt;
+import static mx.unam.iimas.mcic.utils.AsymmetricCryptographicHelper.encrypt;
+
 public class JsonMapper {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -21,5 +26,26 @@ public class JsonMapper {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static byte[] encryptedBytes(Object obj) {
+        String json = toJSONString(obj);
+        try {
+            byte[] jsonBytes = requireNonNull(encrypt(json)).getBytes(UTF_8);
+            return jsonBytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static <T> T decryptedObject(String json, Class<T> tClass) {
+        try {
+            T obj = fromJSONString(decrypt(json), tClass);
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
